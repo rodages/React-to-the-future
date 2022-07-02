f05fecd7ae94c6bba906530bd7e89d42c9240273
rodages <zanasmakarov@gmail.com>
2022-07-02 14:05:52 +0100

    react update

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import {
 } from '@chakra-ui/react';
 import {Nav} from './components/bottom-nav'
 import {Home} from './screens/home'
+import Example from './components/Example';
+import {useState} from 'react'
 
 
 function App() {
+  const [data, setData]=useState("empty component")
   return (
     <ChakraProvider theme={theme}>
+        <Example thiscouldbeanymeaningfulname={"the text that I am passing"}  data={data} setData={setData} />
         <Home />
         <Nav />
     </ChakraProvider>
